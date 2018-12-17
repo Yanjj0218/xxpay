1b911346e9da0e0fbbbdd20d4c9bd190c6a9b875
yanjj0218 <614212208@qq.com>
2018-12-17 14:56:10 +0800

    创建支付模块

---
+++ testpay/src/main/java/com/jk/TestpayApplication.java
@@ -0,0 +1,14 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestpayApplication.class, args);
+    }
+
+}
+
